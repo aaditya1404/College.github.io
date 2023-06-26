df5a6fd2ca9c989b2568e547c304a3cd8bd50647
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2023-06-26 13:56:11 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const express = require("express");
+require("dotenv").config();
+require("./db/conn");
+const app = express();
+const cookieParser = require("cookie-parser");
+const Products = require("./models/productsSchema");
+const DefaultData = require("./defaultdata");
+const cors = require("cors");
+const router = require("./routes/router")
+const productRoutes = require('./routes/router');
+
+app.use(express.json());
+app.use(cookieParser(""));
+app.use(cors());
+app.use(router);
+app.use( productRoutes);
+
+const port = 8005;
+
+app.listen(port,()=>{
+    console.log(`server is running on port number ${port}`);
+});
+
+DefaultData();
